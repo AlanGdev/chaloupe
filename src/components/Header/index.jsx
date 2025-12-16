643139f49977be1378bf8ef616c1daf650ca9e2a
Alan.Grolleau <alan.grolleau@gmail.com>
2025-12-16 20:57:55 +0100

    mise en place du header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,108 @@
+// Header de l'application Chaloupe (daisyUI)
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="navbar bg-base-100 px-4 h-16 md:h-20 lg:h-[100px] items-center">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link href="/">Accueil</Link>
+              </li>
+              <li>
+                <Link href="/restaurant">Restaurant</Link>
+              </li>
+              <li>
+                <Link href="/carte">Carte</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact & horaires</Link>
+              </li>
+            </ul>
+          </div>
+
+          <Link
+            href="/"
+            className="btn btn-ghost normal-case text-xl flex items-center gap-3"
+          >
+            {/* Utilise trois images différentes selon le breakpoint : mobile / tablette / desktop */}
+            <Image
+              src="/logomob.png"
+              alt="La Chaloupe"
+              width={40}
+              height={40}
+              className="block md:hidden"
+            />
+            <Image
+              src="/logotab.png"
+              alt="La Chaloupe"
+              width={64}
+              height={64}
+              className="hidden md:block lg:hidden"
+            />
+            <Image
+              src="/logodesktop.png"
+              alt="La Chaloupe"
+              width={80}
+              height={80}
+              className="hidden lg:block"
+            />
+            <span>La Chaloupe</span>
+          </Link>
+        </div>
+
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link href="/">Accueil</Link>
+            </li>
+            <li>
+              <Link href="/restaurant">Restaurant</Link>
+            </li>
+            <li>
+              <Link href="/carte">Carte</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact & horaires</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navbar-end">
+          <a
+            href="tel:+33687190292"
+            className="btn btn-primary"
+            aria-label="Appeler +33 6 87 19 02 92"
+          >
+            Appeler
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
