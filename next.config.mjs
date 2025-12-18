47fd0a0d6d2aa897b986947bf366efca0ec5566b
AlanGdev <alan.grolleau@gmail.com>
2025-12-18 18:12:35 +0100

    Retrait de l'optimisation des images

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   // Export a fully static HTML site with `next export` semantics
   output: 'export',
   // Disable image optimization at runtime so exported pages work without Next.js server
-  images: {
-    unoptimized: true,
-  },
-};
+}
 
-export default nextConfig;
+export default nextConfig
